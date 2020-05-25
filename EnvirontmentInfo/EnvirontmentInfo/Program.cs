1fbaaea9a27afee4855d37f4f03731f30d224a0e
denis <politsa04@gmail.com>
2020-05-25 12:53:01 +0300

    Первая версия проекта

---
+++ EnvirontmentInfo/EnvirontmentInfo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvirontmentInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "пример для работы с git";
+
+            Console.WriteLine("Привет, мир!");
+            Console.WriteLine($"Компьютер: {Environment.MachineName}, версия ОС: {Environment.OSVersion}");
+
+            Console.WriteLine($"{(new DriveInfo("c:").TotalFreeSpace) / 1024 / 1024:n0} МБайт");
+            Console.Write("\n\n\t");
+        } // Main
+    }
+}
