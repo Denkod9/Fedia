9a85191f329f16d650302eb77950885955f8b84f
denis <politsa04@gmail.com>
2020-05-29 12:36:20 +0300

    Изменение ветвь master

--- EnvirontmentInfo/EnvirontmentInfo/Program.cs
+++ EnvirontmentInfo/EnvirontmentInfo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// Ветвь master
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -17,6 +18,9 @@ namespace EnvirontmentInfo
             Console.WriteLine("Привет, мир!");
             Console.WriteLine($"Компьютер: {Environment.MachineName}, версия ОС: {Environment.OSVersion}");
 
+			// Ветвь master
+			int t = 5;
+			
             Console.WriteLine($"{(new DriveInfo("c:").TotalFreeSpace) / 1024 / 1024:n0} МБайт");
             Console.Write("\n\n\t");
         } // Main
